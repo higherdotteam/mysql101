b17c795ec0794735c2f2fb3da7d5337e448dd545
A Arrow <oneone@gmail.com>
2016-12-23 19:37:19 +0000

    start

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+	fmt.Println("mysql101 by higher.team")
+	fmt.Println("")
+	fmt.Println("http://higher.team is the new MBA")
+	fmt.Println("")
+	fmt.Print("mysql> ")
+
+	time.Sleep(time.Minute)
+}
