91ef01a4c6c27bd1f3d234429d9e1e9494b18c9c
A Arrow <oneone@gmail.com>
2016-12-23 20:05:17 +0000

    tables

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ func main() {
 	fmt.Println("")
 	fmt.Println("http://higher.team is the new MBA")
 	fmt.Println("")
+	fmt.Println("  HINT: type show tables;")
+	fmt.Println("")
 	fmt.Print("mysql> ")
 
 	bio := bufio.NewReader(os.Stdin)
@@ -20,6 +22,15 @@ func main() {
 			break
 		} else if strings.HasSuffix(s, ";") == false {
 			fmt.Println("\nYou forgot the ;\n")
+		} else {
+			fmt.Println("+-------------------+")
+			fmt.Println("| Tables_in_this_db |")
+			fmt.Println("+-------------------+")
+			fmt.Println("| companies         |")
+			fmt.Println("| people            |")
+			fmt.Println("| hackernews        |")
+			fmt.Println("+-------------------+")
+			fmt.Println("3 rows in set (0.00 sec)\n")
 		}
 		fmt.Print("mysql> ")
 		line, _, _ = bio.ReadLine()
